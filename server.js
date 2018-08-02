69d485b4ac42736af4479e1f41a539c56d14e6d4
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2018-08-01 21:12:29 -0300

    Organiza arquivos e inicia um servidor.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/login', function(req, res) {
+    res.render('index');
+});
+
+app.get('/signup', function(req, res) {
+    res.render('registration');
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
