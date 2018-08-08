47d98e1e1671d927f9e248a4c842b47fd91a584e
rodrigo.cortezi <rodrigovcortezi@gmail.com>
2018-08-08 14:52:12 -0300

    Criação do javascript para a página de cadastro (validação de entradas).

---
+++ public/js/registration.js
@@ -0,0 +1,33 @@
+const validator = require('validator');
+
+let formValidator = function(element) {
+    let result;
+    let name = element.name;
+    let value = element.value;
+
+    if(name === 'name') {
+	result = value.length > 0;
+    }
+    else if(name === 'username') {
+	result = validator.isAscii(value);
+    }
+    else if(name === 'email') {
+	result = validator.isEmail(value);
+    }
+    else if(name === 'password') {
+	result = value.length >= 8;
+    }
+    else {
+	result = false;
+    }
+
+    console.log(result);
+    return result;
+};
+
+let inputElements = document.querySelectorAll('.registration-area form input');
+inputElements.forEach(element => {
+    element.addEventListener('blur', function() {
+	let result = formValidator(element);
+    });
+});
